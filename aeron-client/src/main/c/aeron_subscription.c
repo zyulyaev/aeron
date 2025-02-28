0ffca1243a14eac9d0c82ce417b03e1325ff5550
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-02-28 20:41:56 +0100

    [C] Check that an image exists in the Subscription when retaining/releasing.

--- aeron-client/src/main/c/aeron_subscription.c
+++ aeron-client/src/main/c/aeron_subscription.c
@@ -366,10 +366,16 @@ int aeron_subscription_image_retain(aeron_subscription_t *subscription, aeron_im
         return -1;
     }
 
-    aeron_image_incr_refcnt(image);
-
     aeron_image_list_t *volatile image_list;
     AERON_GET_ACQUIRE(image_list, subscription->conductor_fields.image_lists_head.next_list);
+
+    if (-1 == aeron_subscription_find_image_index(image_list, image))
+    {
+        AERON_SET_ERR(EINVAL, "Image not found: correlationId=%" PRIi64, image->key.correlation_id);
+        return -1;
+    }
+
+    aeron_image_incr_refcnt(image);
     aeron_subscription_propose_last_image_change_number(subscription, image_list->change_number);
 
     return 0;
@@ -387,10 +393,16 @@ int aeron_subscription_image_release(aeron_subscription_t *subscription, aeron_i
         return -1;
     }
 
-    aeron_image_decr_refcnt(image);
-
     aeron_image_list_t *volatile image_list;
     AERON_GET_ACQUIRE(image_list, subscription->conductor_fields.image_lists_head.next_list);
+
+    if (-1 == aeron_subscription_find_image_index(image_list, image))
+    {
+        AERON_SET_ERR(EINVAL, "Image not found: correlationId=%" PRIi64, image->key.correlation_id);
+        return -1;
+    }
+
+    aeron_image_decr_refcnt(image);
     aeron_subscription_propose_last_image_change_number(subscription, image_list->change_number);
 
     return 0;
